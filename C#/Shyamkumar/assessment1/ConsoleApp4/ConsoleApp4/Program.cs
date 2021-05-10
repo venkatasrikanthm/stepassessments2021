f91e8bf4caa63d62ad04a07d2dee6de383cdb128
shyamthadi <82875081+shyamthadi@users.noreply.github.com>
2021-05-10 21:41:01 +0530

    Add files via upload

---
+++ C#/Shyamkumar/assessment1/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        private static int sample1, cost = 0;
+        public static int quantity;
+        public static string value;
+        public static int sample2 = 1;
+        public static int Calculation(string item, int quantity)
+        {
+            switch (item)
+            {
+                case "paste":
+                    cost = cost + quantity * 80;
+                    return cost;
+                case "shampoo":
+                    cost = cost + quantity * 120;
+                    return cost;
+                case "chips":
+                    cost = cost + quantity * 30;
+                    return cost;
+                case "milk":
+                    cost = cost + quantity * 30;
+                    return cost;
+                case "rice":
+                    cost = cost + quantity * 800;
+                    return cost;
+                case "drink":
+                    cost = cost + quantity * 40;
+                    return cost;
+                default:
+                    int sample2 = 0;
+                    return sample2;
+            }
+        }
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Minimart:");
+            Console.WriteLine("menu:\n\n");
+            Console.WriteLine("1.paste\t\tprice=80 rs");
+            Console.WriteLine("2.shampoo\tprice=120 rs");
+            Console.WriteLine("3.chips\t\t price=30 rs");
+            Console.WriteLine("4.milk\t\tprice=30 rs");
+            Console.WriteLine("5.rice\t\tprice=800 rs");
+            Console.WriteLine("6.drink\t\tprice=40 rs");
+            while (sample1 == 0)
+            {
+                Console.WriteLine("would you like to buy any item: ");
+                string wish = Console.ReadLine();
+                if (wish == "no")
+                {
+                    Console.WriteLine("thank you ");
+                    sample1 = 1;
+                }
+                else if (wish == "yes")
+                {
+                    while (sample2 == 1)
+                    {
+                        Console.WriteLine("what would you like to buy:");
+                        string item = Console.ReadLine();
+                        if (item == "paste" || item == "shampoo" || item == "rice" || item == "milk" || item == "chips" || item == "drink")
+                        {
+                            while (sample2 == 1)
+                            {
+                                Console.WriteLine("please enter quantity: ");
+                                value = Console.ReadLine();
+                                int parsevariable;
+                                if (int.TryParse(value, out parsevariable))
+                                {
+                                    quantity = Convert.ToInt32(value);
+                                    int cost = Calculation(item, quantity);
+                                    while (sample2 == 1)
+                                    {
+                                        Console.WriteLine("your bill: " + cost);
+                                        Console.WriteLine("do you need anything Wish: ");
+                                        string Wish = Console.ReadLine();
+                                        if (Wish == "no")
+                                        {
+                                            break;
+                                        }
+                                        else if (Wish == "yes")
+                                        {
+                                            Console.WriteLine("what item needed: ");
+
+                                            string Item = Console.ReadLine();
+                                            Console.WriteLine("please enter quantity: ");
+                                            value = Console.ReadLine();
+                                            int parsevariable2;
+                                            if (int.TryParse(value, out parsevariable2))
+                                            {
+                                                quantity = Convert.ToInt32(value);
+                                                cost = Calculation(Item, quantity);
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine("invalid  say integers");
+                                            }
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("invalid say yes or no ");
+                                        }
+                                    }
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("invalid  say integer ");
+                                }
+                            }
+                            Console.WriteLine("Thank you total bill: " + cost);
+                            return sample2 = 0;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid say menu items only ");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("invalid say yes or no");
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ C#/Shyamkumar/assessment1/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asssessment1
+{
+    class Algorithm
+    {
+        public static string Encrypt(string input)
+        {
+            string sample1 = "";
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                sample1 += input[i];
+            }
+            sample1 = sample1.Replace("a", "0");
+            sample1 = sample1.Replace("e", "1");
+            sample1 = sample1.Replace("i", "2");
+            sample1 = sample1.Replace("o", "3");
+            sample1 = sample1.Replace("u", "4");
+            string encrypt = sample1 + "aca";
+            return encrypt;
+        }
+        public static string Decrypt(string input)
+        {
+            string sample1 = "";
+            for (int i = input.Length - 4; i >= 0; i--)
+            {
+                sample1 += input[i];
+            }
+            sample1 = sample1.Replace("0", "a");
+            sample1 = sample1.Replace("1", "e");
+            sample1 = sample1.Replace("2", "i");
+            sample1 = sample1.Replace("3", "o");
+            sample1 = sample1.Replace("4", "u");
+            return sample1;
+        }
+        static int Main(string[] args)
+        {
+            int real = 1;
+            while (real == 1)
+            {
+                Console.WriteLine("enter encrypt or decrypt or exit:");
+
+                string encrypt = Console.ReadLine();
+                if (encrypt == "exit")
+                {
+                    real = 0;
+                    return real;
+                }
+                else if (encrypt == "encrypt" || encrypt == "decrypt")
+                {
+                    Console.WriteLine("enter text:");
+                    string str = Console.ReadLine();
+
+                    switch (encrypt)
+                    {
+                        case "encrypt":
+                            string str1 = Encrypt(str);
+                            Console.WriteLine("encrypted:" + str1);
+                                                        break;
+                        case "decrypt":
+                            string str2 = Decrypt(str);
+                            Console.WriteLine("decrypted original text:" + str2);
+                            break;
+                        case "exit":
+                            real = 0;
+                            return real;
+                        default:
+                            Console.WriteLine("enter encrypt or decrypt or exit:");
+                            break;
+                    }
+                }
+            }
+                       return 0;
+        }
+    }
+
+}
