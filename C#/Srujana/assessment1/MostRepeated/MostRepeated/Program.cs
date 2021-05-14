bc05dc733a10d8f4a722870b057dea5e4b66a408
SrujanaKamireddi <76680126+SrujanaKamireddi@users.noreply.github.com>
2021-05-14 15:32:54 +0530

    Add files via upload

---
+++ C#/Srujana/assessment1/MostRepeated/MostRepeated/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MostRepeated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] StrArray = new string[10];
+            Dictionary<string, int> WordCount = new Dictionary<string, int>();
+
+            int i;
+            Console.WriteLine("Enter the lenght of the array: ");
+            int length = int.Parse(Console.ReadLine());
+
+            //Reading Array of String
+
+            for (i = 0; i < length; i++)
+            {
+                StrArray[i]=Console.ReadLine();
+
+            }
+
+            // Counting the frquencies of string 
+
+            for (i = 0; i < length; i++)
+            {
+                if (WordCount.ContainsKey(StrArray[i]))
+                {
+                    WordCount[StrArray[i]] += 1;
+                }
+                else
+                {
+                    WordCount.Add(StrArray[i],1);
+                }
+            }
+            
+            // printing the word and its respective count
+
+            foreach(KeyValuePair<string,int> WordCountPair in WordCount)
+            {
+                Console.WriteLine("{0}:{1}", WordCountPair.Key, WordCountPair.Value);
+            }
+
+            //Highest Frequency
+
+            String Word = "";
+            int count = 0;
+            foreach(KeyValuePair<string, int> keyValue in WordCount)
+            {
+                if (count < keyValue.Value)
+                {
+                    count = keyValue.Value;
+                    Word = keyValue.Key;
+                }
+            }
+            
+            Console.WriteLine("Most Frequent Word = {0} {1}" ,Word,count);
+        }
+    }
+}
