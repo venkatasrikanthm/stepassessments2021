bedd4b6528641ee8d58550f70d18285310da1a4b
SrujanaKamireddi <76680126+SrujanaKamireddi@users.noreply.github.com>
2021-05-14 15:34:17 +0530

    Add files via upload

---
+++ C#/Srujana/assessment1/ValidCovidVaccine/ValidCovidVaccine/PersonalDetails.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ValidCovidVaccine
+{
+    class PersonalDetails
+    {
+        string _name;
+        string _aadharnum;
+        int _age;
+        string _gender;
+
+        public PersonalDetails(string Name,string Aadharnum,int Age, string Gender)
+        {
+            this._name=Name;
+            this._aadharnum=Aadharnum;
+            this._age=Age;
+            this._gender = Gender;
+        }
+
+        public void Eligible(string Name, string Aadharnum, int Age, string Gender)
+        {
+            if (Gender == "female")
+            {
+                Console.WriteLine("Are You Pregnant yes or no?");
+                string pregnant = Console.ReadLine();
+                if (pregnant == "yes")
+                {
+                    if (Age >= 30 && Age <= 40)
+                    {
+                        Console.WriteLine("{0} is eligible for Covaxin", Name);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} is not eligible for vaccine", Name);
+                    }
+                }
+                else
+                {
+                    goto ageCheck;
+                }
+            }
+            else
+            {
+                goto ageCheck;
+            }
+            ageCheck:
+
+                if (Age < 18)
+                {
+                    Console.WriteLine("{0} is not eligible for vaccine", Name);
+                }
+                else if (Age >= 18 && Age <= 45)
+                {
+                    Console.WriteLine("{0} is eligible for Covi Shield", Name);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is eligible for Covaxin ", Name);
+                }
+        }
+    }
+}
---
+++ C#/Srujana/assessment1/ValidCovidVaccine/ValidCovidVaccine/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ValidCovidVaccine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter your Aadhar number: ");
+            string Aadharnum = Console.ReadLine();
+            Console.WriteLine("Enter your age: ");
+            int Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your Gender male or femlale");
+            string Gender = Console.ReadLine();
+
+            PersonalDetails person = new PersonalDetails(Name,Aadharnum,Age,Gender);
+
+            person.Eligible(Name,Aadharnum,Age,Gender);
+        }
+       
+    }
+}
